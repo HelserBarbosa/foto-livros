c297855b604bd3554ccca6d90f372d6bbc022950
HelderBarbosa <helderlb@gmail.com>
2019-01-03 20:04:02 -0300

    javadoc

--- back/projeto.fotolivro/src/main/java/projeto/fotolivro/core/models/EntidadeBase.java
+++ back/projeto.fotolivro/src/main/java/projeto/fotolivro/core/models/EntidadeBase.java
@@ -1,7 +1,12 @@
 package projeto.fotolivro.core.models;
 
 import java.io.Serializable;
-
+/**
+ * Todas entidades devem extender da entiadade base
+ * 
+ * @author helderlb@gmail.com
+ *
+ */
 public abstract class EntidadeBase implements Serializable{
 
     private static final long serialVersionUID = 1L;
