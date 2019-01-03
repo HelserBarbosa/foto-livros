9a1bdd993743c0917c9110c9b966a48fd1dd8917
HelderBarbosa <helderlb@gmail.com>
2019-01-03 20:02:12 -0300

    Entidade base

---
+++ back/projeto.fotolivro/src/main/java/projeto/fotolivro/core/models/EntidadeBase.java
@@ -0,0 +1,9 @@
+package projeto.fotolivro.core.models;
+
+import java.io.Serializable;
+
+public abstract class EntidadeBase implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+}
